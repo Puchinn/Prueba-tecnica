35a7611a7297e43f219aba68712ceded15e3b815
Esteban Sayago <sayagoo823@gmail.com>
2023-03-23 11:18:03 -0300

    agregar validacion del input

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function useInput() {
     const [value, setValue] = useState('')
-    return { value, setValue }
+    const [error, setError] = useState('')
+    useEffect(() => {
+        if (/^ /.test(value)) return setError('El texto no puede comenzar con un espacio.')
+        if (value.length !== 0 && value.length < 2) return setError('El texto tiene que ser mayor o igual a dos caracteres')
+        if (value.length !== 0 && value.length > 50) return setError('Maximo de 50 caracteres')
+        if (value.length !== 0 && !/^[a-zA-Z0-9 ]+$/.test(value)) return setError('El texto no puede contener caracteres especiales')
+        setError('')
+    }, [value])
+    return { value, setValue, error }
 }
 
 export default useInput
