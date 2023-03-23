e8d96c639b8920408af073defd420dba1f8b5332
Esteban Sayago <sayagoo823@gmail.com>
2023-03-23 11:18:33 -0300

    eliminar bloques try catch

--- src/hooks/useSearchMovies.js
+++ src/hooks/useSearchMovies.js
@@ -3,11 +3,14 @@ import getSearch from "../services/getSearch";
 
 function useSearchMovies() {
     const [movies, setMovies] = useState([])
+    const [searching, setSearching] = useState(false)
     const searchMovies = async (busqueda) => {
+        setSearching(true)
         const movies = await getSearch(busqueda)
         setMovies(movies)
+        setSearching(false)
     }
-    return { movies, searchMovies }
+    return { movies, searchMovies, searching }
 }
 
 export default useSearchMovies 
