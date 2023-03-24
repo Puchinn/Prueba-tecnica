daa9a8048f063f7c5e5fd967631babece8029ad2
Esteban Sayago <sayagoo823@gmail.com>
2023-03-24 10:49:48 -0300

    crear componente

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,10 @@
+export default function Pagination() {
+	return (
+		<div className='flex justify-center py-5'>
+			<div className='btn-group grid grid-cols-2'>
+				<button className='btn-outline btn'>Previous page</button>
+				<button className='btn-outline btn'>Next</button>
+			</div>
+		</div>
+	)
+}
