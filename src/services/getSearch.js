84aef209f4c25f5d90b6539ac3007bce47d1f64d
Esteban Sayago <sayagoo823@gmail.com>
2023-03-25 13:31:36 -0300

    modificar servicio para retornar todos los datos

--- src/services/getSearch.js
+++ src/services/getSearch.js
@@ -1,18 +1,23 @@
 const { VITE_API_KEY } = import.meta.env
+const URL = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_API_KEY}&language=en-US`
 
-const getSearch = async (busqueda) => {
-    const response = await fetch
-        (`https://api.themoviedb.org/3/search/movie?api_key=${VITE_API_KEY}&language=en-US&query=${busqueda}&page=1`)
-
+const getSearch = async (string, num) => {
+    const validateString = typeof string === 'string' ? '&query=' + string : ''
+    const validateNum = isNaN(num) ? '&page=' + 1 : '&page=' + num
+    const query = validateString + validateNum
+    const response = await fetch(URL + query)
     const json = await response.json()
-    const movies = json.results
-    return movies?.map(movie => ({
-        id: movie.id,
-        title: movie.title,
-        date: movie.release_date,
-        rate: movie.vote_average,
-        image: movie.poster_path
-    }))
+    return json
 }
 
 export default getSearch
+
+
+
+// const movies = json.results?.map(movie => ({
+//     id: movie.id,
+//     title: movie.title,
+//     date: movie.release_date,
+//     rate: movie.vote_average,
+//     image: movie.poster_path
+// }))
