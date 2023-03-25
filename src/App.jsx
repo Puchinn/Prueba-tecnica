9e810d8bfba18ea96a7ffc1d5c4aaa84d98d412a
Esteban Sayago <sayagoo823@gmail.com>
2023-03-25 13:32:12 -0300

    agregar footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 function App() {
 	return (
 		<div className='App' data-theme='winter'>
-			<div className='flex h-screen flex-col bg-base-200'>
+			<div className='min-h-[80vh]'>
 				<Nav />
 				<Outlet />
 			</div>
+			<Footer />
 		</div>
 	)
 }
