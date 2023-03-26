4268938e20008cab797c2f10fa0d5fca16567f6f
Esteban Sayago <sayagoo823@gmail.com>
2023-03-25 23:31:03 -0300

    cambiar lenguaje a español para las busquedas

--- src/services/getSearch.js
+++ src/services/getSearch.js
@@ -1,5 +1,5 @@
 const { VITE_API_KEY } = import.meta.env
-const URL = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_API_KEY}&language=en-US`
+const URL = `https://api.themoviedb.org/3/search/movie?api_key=${VITE_API_KEY}&language=es`
 
 const getSearch = async (string, num) => {
     const validateString = typeof string === 'string' ? '&query=' + string : ''
@@ -11,13 +11,3 @@ const getSearch = async (string, num) => {
 }
 
 export default getSearch
-
-
-
-// const movies = json.results?.map(movie => ({
-//     id: movie.id,
-//     title: movie.title,
-//     date: movie.release_date,
-//     rate: movie.vote_average,
-//     image: movie.poster_path
-// }))
