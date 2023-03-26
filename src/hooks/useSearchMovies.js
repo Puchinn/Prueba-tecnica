898761190127cadd8c08453286042aefdc32493e
Esteban Sayago <sayagoo823@gmail.com>
2023-03-26 01:10:54 -0300

    agregar logica para buscar y hacer paginacion de peliculas

--- src/hooks/useSearchMovies.js
+++ src/hooks/useSearchMovies.js
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import getSearch from "../services/getSearch";
 
-function useSearchMovies() {
-    const [movies, setMovies] = useState([])
-    const [searching, setSearching] = useState(false)
-    const searchMovies = async (busqueda) => {
-        setSearching(true)
-        const movies = await getSearch(busqueda)
-        setMovies(movies)
-        setSearching(false)
-    }
-    return { movies, searchMovies, searching }
+function useSearchMovies(initialQuery) {
+    const [movies, setMovies] = useState([]);
+    const [actualPage, setActualPage] = useState(1);
+    const [pages, setPages] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getData = useCallback(async (string, pag) => {
+        setIsLoading(true);
+        const { results, page, total_pages } = await getSearch(string, pag);
+        const movies = results.map(movie => ({
+            id: movie.id,
+            title: movie.title,
+            date: movie.release_date,
+            rate: movie.vote_average,
+            image: movie.poster_path
+        }));
+        setMovies(movies);
+        setActualPage(page);
+        setPages(total_pages);
+        setIsLoading(false);
+    })
+
+    useEffect(() => {
+        getData(initialQuery, 1);
+    }, [initialQuery]);
+
+    useEffect(() => {
+        getData(initialQuery, actualPage);
+    }, [actualPage]);
+
+    return { movies, actualPage, pages, setActualPage, isLoading };
 }
 
-export default useSearchMovies 
+export default useSearchMovies
