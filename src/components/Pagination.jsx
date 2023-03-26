be11790895159d028b52a516b8043fcb577460f1
Esteban Sayago <sayagoo823@gmail.com>
2023-03-25 23:46:06 -0300

    agregar logica para pasar de paginas

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,29 @@
-export default function Pagination() {
+export default function Pagination({ pages, actualPage, setActualPage }) {
+	const next = () => {
+		if (actualPage + 1 > pages) {
+			setActualPage(1)
+			return
+		}
+		setActualPage(actualPage + 1)
+	}
+
+	const back = () => {
+		if (actualPage - 1 === 0) {
+			setActualPage(pages)
+			return
+		}
+		setActualPage(actualPage - 1)
+	}
+
 	return (
 		<div className='flex justify-center py-5'>
 			<div className='btn-group grid grid-cols-2'>
-				<button className='btn-outline btn'>Previous page</button>
-				<button className='btn-outline btn'>Next</button>
+				<button onClick={back} className='btn-outline btn'>
+					Previous page
+				</button>
+				<button onClick={next} className='btn-outline btn'>
+					Next
+				</button>
 			</div>
 		</div>
 	)
