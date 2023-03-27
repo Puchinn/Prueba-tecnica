aaa37aabc800d605716107638cc843a90189d1f5
Esteban Sayago <sayagoo823@gmail.com>
2023-03-27 12:28:16 -0300

    agregar letra 'ñ' a la validacion

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -7,7 +7,7 @@ function useInput() {
         if (/^ /.test(value)) return setError('El texto no puede comenzar con un espacio.')
         if (value.length !== 0 && value.length < 2) return setError('El texto tiene que ser mayor o igual a dos caracteres')
         if (value.length !== 0 && value.length > 50) return setError('Maximo de 50 caracteres')
-        if (value.length !== 0 && !/^[a-zA-Z0-9 ]+$/.test(value)) return setError('El texto no puede contener caracteres especiales')
+        if (value.length !== 0 && !/^[a-zA-Z0-9 ñ*]+$/.test(value)) return setError('El texto no puede contener caracteres especiales')
         setError('')
     }, [value])
     return { value, setValue, error }
