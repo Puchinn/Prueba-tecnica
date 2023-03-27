cda2196749ea8b172956788936f8c3b67a309cd9
Esteban Sayago <sayagoo823@gmail.com>
2023-03-27 12:26:58 -0300

    agregar una alerta si no se encuentran resultados

--- src/components/ListOfMovies.jsx
+++ src/components/ListOfMovies.jsx
@@ -1,11 +1,11 @@
 import Movie from './Movie'
 
-function ListOfMovies({ movies }) {
+function ListOfMovies({ movies, isLoading }) {
 	const isArray = Array.isArray(movies)
 	const hasMovies = isArray && movies.length >= 1
 	return (
 		<div>
-			{hasMovies ? (
+			{hasMovies && !isLoading && (
 				<ul className='mx-auto grid max-w-7xl grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-10 p-2'>
 					{movies.map((movie) => (
 						<Movie
@@ -16,8 +16,25 @@ function ListOfMovies({ movies }) {
 							rate={movie.rate}></Movie>
 					))}
 				</ul>
-			) : (
-				<p>No se encontraron peliculas</p>
+			)}
+			{!hasMovies && !isLoading && (
+				<div className='alert alert-warning mx-auto max-w-lg'>
+					<div>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-6 w-6 flex-shrink-0 stroke-current'
+							fill='none'
+							viewBox='0 0 24 24'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
+							/>
+						</svg>
+						<span>No se encontraron peliculas!</span>
+					</div>
+				</div>
 			)}
 		</div>
 	)
